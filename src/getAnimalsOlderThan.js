770240bf1f8f72ec61a5fbef6ebeda9570b879d9
Vinicius Kaminski <vikaminski@hotmail.com>
2022-06-29 16:19:58 -0300

    Requisito 1 ao 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const containerAnimal = data.species.filter((element) => element.name === animal);
+  const [{ residents }] = containerAnimal;
+  return residents.every((element) => (element.age >= age));
 }
+
+console.log(getAnimalsOlderThan('lions', 15));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length === 1) {
+    return data.species.filter((element) => element.id.includes(ids));
+  }
+  if (ids.length > 1) {
+    return data.species.filter((element) => ids.some((elementtwo) => elementtwo === element.id));
+  }
 }
 
 module.exports = getSpeciesByIds;
